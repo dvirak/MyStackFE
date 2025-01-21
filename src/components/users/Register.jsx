63dd6b833bc658c4d7cfed50146960e4c286246c
dvirak <dvirak43@vt.edu>
2025-01-21 18:46:46 -0500

    Stylized Register Section

--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -27,27 +27,30 @@ export default function Register() {
 
   return (
     <form
+      className="register-form"
       onSubmit={(e) =>
         registerSubmit(e, userData, setErrorMessage, setIsLoading)
       }
     >
       <h3>Register</h3>
 
-      {/* Render input fields dynamically from userData, excluding password field */}
-      {Object.keys(userData).map((key) =>
-        key === "password" ? null : (
-          <InputField
-            key={key}
-            fieldName={key}
-            userData={userData}
-            setUserData={setUserData}
-          />
-        )
-      )}
+      <div className="register-input-grid">
+        {/* Render input fields dynamically from userData, excluding password field */}
+        {Object.keys(userData).map((key) =>
+          key === "password" ? null : (
+            <InputField
+              key={key}
+              fieldName={key}
+              userData={userData}
+              setUserData={setUserData}
+            />
+          )
+        )}
+      </div>
 
       {/* Submit button for registering the user */}
-      <button>
-        <span className="login-button">Register</span>
+      <button className="login-button">
+        <span>Register</span>
       </button>
     </form>
   );
